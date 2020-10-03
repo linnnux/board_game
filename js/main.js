bedd4bf7604c69259f4767eb055874ecac9050b9
Nacim MEZIANI <mmn@2mn.info>
2020-10-03 22:05:47 +0200

    :v 0.0.6: le plateau est généré dynamiquement en précisant le min et le max de la hauteur et de la largeur depuis le main

--- js/main.js
+++ js/main.js
@@ -2,9 +2,13 @@
 
 // get the reference for the body
 var body = document.getElementsByTagName("body")[0];
+var maxLenght = 10;
+var minLenght = 5;
 
+var height = Math.floor(Math.random() * maxLenght)+minLenght;
+var width  = Math.floor(Math.random() * maxLenght)+minLenght;
 /*
 Board(height,weight,bodyReference)
 */
-plateau = new Board(5, 5, body);
+plateau = new Board(height, width, body);
 plateau.generateTableau;
