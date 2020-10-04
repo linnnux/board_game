4138c57b90c9aa1f5d1bddb924dbf1ebbcf8d8ed
Nacim MEZIANI <mmn@2mn.info>
2020-10-04 14:54:12 +0200

    :0.0.7: amélioration du code, la largeur et la hauteur pour générer les cases colorées est mieux controlée, les valeurs randoms sont obtenues proprement en utilisant une fonction getRanInteger()

--- js/Board.js
+++ js/Board.js
@@ -8,7 +8,6 @@
     https://developer.mozilla.org/fr/docs/Web/API/Element/setAttribute
     https://developer.mozilla.org/fr/docs/Web/API/Element/getAttribute
     https://www.w3schools.com/jsref/met_element_getattribute.asp
-
 */
 
 
@@ -18,82 +17,167 @@ class Board
 
     constructor(body)
     {
-      // get the reference for the body
+      //déclaration et initialisation des variables
 
+      // taille tab max
+      this._maxLenght    = 10;
+      // taille tab min
+      this._minLenght    = 5;
+
+      // longueur x tab
+      this._xTableLength = 0;
+      // longueur y tab
+      this._xTableLength = 0
+
+      // générer un tableau (plateau)
+      this.generateTable(body);
+
+    }
+
+    generateTable(body)
+    {
       // creates a <table> element and a <tbody> element
       var tbl     = document.createElement("table");
       var tblBody = document.createElement("tbody");
 
-      this._maxLenght = 10;
-      this._minLenght = 5;
-
-      this._x = Math.floor(Math.random() * this._maxLenght)+this._minLenght;;
-      this._y = Math.floor(Math.random() * this._maxLenght)+this._minLenght;;
-
+      // générer les tailles du plateau entre le max et le minim défini
+      this._xTableLength = this.getRndInteger( this._minLenght,  this._maxLenght);
+      this._yTableLength = this.getRndInteger( this._minLenght,  this._maxLenght);
 
       // creating all cells
 
-      for (var i = 0; i < this._x; i++)
+      for (var i = 0; i < this._xTableLength; i++)
       {
 
         // creates a table row
         var row = document.createElement("tr");
 
-        for (var j = 0; j < this._y; j++)
-        {
+          for (var j = 0; j < this._yTableLength; j++)
+          {
 
-          // Create a <td> element and a text node, make the text
-          // node the contents of the <td>, and put the <td> at
-          // the end of the table row
-          var cell = document.createElement("td");
-          var cellText = document.createTextNode("row "+i+", column "+j);
-          var cellText = document.createTextNode("row "+i+", column "+j);
-          cell.setAttribute("x", i);
-          cell.setAttribute("y", j);
-          cell.appendChild(cellText);
-          row.appendChild(cell);
+            // Create a <td> element and a text node, make the text
+            // node the contents of the <td>, and put the <td> at
+            // the end of the table row
+            var cell = document.createElement("td");
+            var cellText = document.createTextNode("row "+i+", column "+j);
+            var cellText = document.createTextNode("row "+i+", column "+j);
+            cell.setAttribute("x", i);
+            cell.setAttribute("y", j);
+            cell.appendChild(cellText);
+            row.appendChild(cell);
 
-        }
-        // add the row to the end of the table body
-        try
-        {
-          tblBody.appendChild(row);
-        }
-        catch (e)
-        {
-          console.log("erreur création tableau");
-        }
+          }
+          // add the row to the end of the table body
+          try
+          {
+            tblBody.appendChild(row);
+          }
+          catch (e)
+          {
+            console.log("erreur création tableau");
+            return false
+          }
 
         // put the <tbody> in the <table>
         tbl.appendChild(tblBody);
 
         // appends <table> into <body>
         body.appendChild(tbl);
+
         // sets the border attribute of tbl to 2;
         tbl.setAttribute("border", "5");
     }
+  }
 
-
-    }
-
-
-    set height(x)
+    /*
+      Cette fonction génère les cases inaccessibles
+    */
+    setBackground(body,color,nbCase)
     {
-      this._x = x;
-    }
-    get height()
-    {
-      return this._x;
+
+      if (nbCase > this._xTableLength || nbCase > this._yTableLength)
+      {
+        nbcase = this._xTableLength;
+      }
+
+      try
+      {
+        for(let i=0;i< nbCase; i++)
+        {
+
+          this._randomX = this.getRndInteger(0, this._xTableLength);
+          this._randomY = this.getRndInteger(0, this._yTableLength);
+
+          this.setCase(body, this._randomX, this._randomY, color);
+        }
+      }
+      catch (e)
+      {
+        console.log('setBackground case access error');
+        return false;
+      }
+
     }
 
-    set maxLenght(maxLenght)
+    /*
+      Cette fonction prend 3 paramètres body,x,y, accède à la position du tableau et change sa couleur
+      body = réfèrence vers le body de la page html
+      x     : type int , indique la position horizontale
+      y     : type int , indique position verticale
+      color : type str , indique la couleur
+    */
+    setCase(body,x,y,color)
     {
-      this._maxLenght = maxLenght;
-    }
-    get maxLenght()
-    {
-      return this._maxLenght;
+      // récupère une liste de tous les éléments body (il n'y en aura qu'un),
+      // et sélectionne le premier (indice 0) de ces éléments
+
+      try
+      {
+        this._body = body;
+      }
+      catch (e)
+      {
+        alert('no body created');
+        return false;
+      }
+
+      // à présent, trouve tous les éléments p enfants de cet élément body
+      try
+      {
+        this._bodyElements = this._body.getElementsByTagName("td");
+      }
+      catch (e)
+      {
+        alert('no table exists');
+      }
+
+
+      let cellule = this._bodyElements[0];
+
+      // recherche une case spécifique grâce à ses attributs x et y
+      if(cellule.hasAttribute)
+      {
+        for(let i =0; i< this._bodyElements.length; i++)
+        {
+            cellule = this._bodyElements[i];
+            if(cellule.getAttribute("x")==x && cellule.getAttribute("y")==y)
+            {
+              const align = "x = "+cellule.getAttribute("x")+" y = "+cellule.getAttribute("y");
+              //alert(align); // affiche la valeur de l'attribut align pour l'élément dont l'id="div1"
+              cellule.style.background = color;
+           }
+        }
+
+      }
+
     }
 
 
+    // return une valeur aléatoire entre min et max
+    getRndInteger(min, max)
+    {
+      return Math.floor(Math.random() * (max - min + 1) ) + min;
+    }
+
+    
 }
--- js/main.js
+++ js/main.js
@@ -1,9 +1,21 @@
-//main program file//
+//main
+
+// initialisation des paramètres
 
 // get the reference for the body
 var body = document.getElementsByTagName("body")[0];
 
+// nombre max de case inaccéssibles à génrer
+let nbCase = 5;
+
+// couleur de case
+let color = "green";
+
 /*
 Board(bodyReference)
 */
+
+// initialisation du plateau
 plateau = new Board(body);
+// générer les cases inaccessibles
+plateau.setBackground(body,color,nbCase);
