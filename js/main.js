d358ea7ef1a1c2c7a2e922e4c5d87eb36095e110
Nacim MEZIANI <mmn@2mn.info>
2020-10-05 08:32:45 +0200

    :v 0.0.12: ajoute la possibilité de se déplacer avec les flèches du clavier

--- js/main.js
+++ js/main.js
@@ -97,3 +97,81 @@ function move(event)
   plateau.setCase(body,actualX,actualY,"red");
 
 }
+
+
+// source : https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent/key
+window.addEventListener("keydown", function (event) {
+  if (event.defaultPrevented) {
+    return; // Do nothing if the event was already processed
+  }
+
+  switch (event.key) {
+    case "Down": // IE/Edge specific value
+    case "ArrowDown":
+      // Do something for "down arrow" key press.
+        if(actualX + 1 < plateau.xTableLength && plateau.isCaseAvailable(body, actualX+1, actualY) == true )
+        {
+
+            plateau.setCase(body,actualX,actualY,'#'+color,true);
+            actualX = actualX+1;
+        }
+        else
+        {
+          alert('cellule non inaccessible');
+        }
+
+      break;
+    case "Up": // IE/Edge specific value
+    case "ArrowUp":
+      // Do something for "up arrow" key press.
+      if( (actualX - 1) >= 0 && plateau.isCaseAvailable(body, actualX-1, actualY)== true)
+      {
+          plateau.setCase(body,actualX,actualY,'#'+color,true)
+          actualX = actualX-1;
+      }
+      else
+      {
+        alert('cellule non inaccessible');
+      }
+      break;
+    case "Left": // IE/Edge specific value
+    case "ArrowLeft":
+      // Do something for "left arrow" key press.
+      if( (actualY - 1) >= 0 && plateau.isCaseAvailable(body, actualX, actualY-1) == true)
+      {
+        plateau.setCase(body,actualX,actualY,'#'+color,true);
+        actualY = actualY-1;
+      }
+      else
+      {
+        alert('cellule non inaccessible');
+      }
+
+      break;
+    case "Right": // IE/Edge specific value
+    case "ArrowRight":
+      // Do something for "right arrow" key press.
+      if(actualY + 1 < plateau.yTableLength && plateau.isCaseAvailable(body, actualX, actualY+1) == true)
+      {
+        plateau.setCase(body,actualX,actualY,'#'+color,true);
+        actualY = actualY+1;
+      }
+      else
+      {
+        alert('cellule non inaccessible');
+      }
+      break;
+    case "Enter":
+      // Do something for "enter" or "return" key press.
+      break;
+    case "Esc": // IE/Edge specific value
+    case "Escape":
+      // Do something for "esc" key press.
+      break;
+    default:
+      return; // Quit when this doesn't handle the key event.
+  }
+  plateau.setCase(body,actualX,actualY,"red");
+  // Cancel the default action to avoid it being handled twice
+  event.preventDefault();
+}, true);
